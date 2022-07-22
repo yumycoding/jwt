98656041741e0eee3cd3f7dcaf110777918a8e5d
yumycoding <faisalshahzad@yumyapps.com>
2022-07-22 06:39:51 -0700

    with build

--- src/main/java/com/yumyapps/jwt/JwtApplication.java
+++ src/main/java/com/yumyapps/jwt/JwtApplication.java
@@ -1,9 +1,13 @@
 package com.yumyapps.jwt;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.List;
 
 @SpringBootApplication
 public class JwtApplication {
